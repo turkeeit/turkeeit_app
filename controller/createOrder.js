b9b1be1cdea7494c23872514e7ac512aa5e66840
turkeeit <myevaservices@gmail.com>
2025-05-17 10:09:38 +0000

    changes

--- controller/createOrder.js
+++ controller/createOrder.js
@@ -1,7 +1,6 @@
-const express = require("express");
+require("dotenv").config();
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
-require("dotenv").config();
 const connection = require("../config/dbconfig");
 const { v4: uuidv4 } = require("uuid");
 
